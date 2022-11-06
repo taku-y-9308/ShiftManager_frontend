d654a6d968978f3a8bffd7eb62f5fd828f206171
takumi <y.takumi4@gmail.com>
2022-11-06 22:35:32 +0900

    ShiftTimeLineView.vueからdataをプロパティとして渡せるように変更

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,19 +1,10 @@
 <script setup>
+    import { defineProps  } from 'vue';
     import { GChart } from 'vue-google-charts';
+    defineProps({
+        data:Object
+    })
 
-    const columns = [
-        { type: 'string', id: 'President' },
-        { type: 'date', id: 'Start' },
-        { type: 'date', id: 'End' },
-    ];
-
-    const rows = [
-        ['Washington', new Date(1789, 3, 30), new Date(1797, 2, 4)],
-        ['Adams', new Date(1797, 2, 4), new Date(1801, 2, 4)],
-        ['Jefferson', new Date(1801, 2, 4), new Date(1809, 2, 4)],
-    ];
-
-    const data = [columns, ...rows];
     const settings = {
         packages: ['timeline'],
     }
--- src/views/ShiftTimeLineView.vue
+++ src/views/ShiftTimeLineView.vue
@@ -1,6 +1,22 @@
 <script setup>
     import TimeLine from '../components/TimeLine.vue'
+
+    const columns = [
+        { type: 'string', id: 'President' },
+        { type: 'date', id: 'Start' },
+        { type: 'date', id: 'End' },
+    ];
+
+    const rows = [
+        ['Washington', new Date(1789, 3, 30), new Date(1797, 2, 4)],
+        ['Adams', new Date(1797, 2, 4), new Date(1801, 2, 4)],
+        ['Jefferson', new Date(1801, 2, 4), new Date(1809, 2, 4)],
+    ];
+
+    const data = [columns, ...rows];
 </script>
 <template>
-    <TimeLine />
+    <TimeLine 
+        :data="data"
+    />
 </template>
