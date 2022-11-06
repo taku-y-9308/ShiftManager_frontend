599e4454e7e2792f5508ad714a9b407e00797cc6
takumi <y.takumi4@gmail.com>
2022-11-06 22:47:17 +0900

    日付入力用ボックスと各種ボタンを追加

--- src/views/ShiftTimeLineView.vue
+++ src/views/ShiftTimeLineView.vue
@@ -14,9 +14,23 @@
     ];
 
     const data = [columns, ...rows];
+    console.log(data)
 </script>
+
 <template>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <input type="date" class="form-control"  placeholder="タップして日付を入力" id="edit-date">
+            </div>
+            <button type="button" class="btn btn-primary mr-3" id="submit-date">送信</button>
+            <button type="button" class="btn btn-secondary mr-3" id="create-newshift">新規シフト作成</button>
+            <button type="button" class="btn btn-danger" id="open_publish_shift_modal">シフト公開</button>
+        </div>
+    </div>
+
     <TimeLine 
         :data="data"
+        class="mt-4"
     />
 </template>
